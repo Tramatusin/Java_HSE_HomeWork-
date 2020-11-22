bfe9c481851d63f67e862f975ff0db115a291cfc
AchekSlime <impsface@yandex.ru>
2020-11-22 23:26:20 +0300

    Проверил Асатиани Тимур

--- MyArrayList/src/ru/hse/MyArrayList/IMyArrayList.java
+++ MyArrayList/src/ru/hse/MyArrayList/IMyArrayList.java
@@ -1,5 +1,8 @@
 package ru.hse.MyArrayList;
 
+// FIXME почему интерфейс конкртено для ArrayList а не для всех листов?
+// FIXME нужен интерфейс List который будут имплементировать и ArrayList и LinkedLisst.
+// TODO нет комментариев для каждого метода.
 public interface IMyArrayList<T> {
     T getItem(int index);
     void putItem(T value);
--- MyArrayList/src/ru/hse/MyArrayList/MyArrayList.java
+++ MyArrayList/src/ru/hse/MyArrayList/MyArrayList.java
@@ -1,9 +1,10 @@
 package ru.hse.MyArrayList;
 
-public class MyArrayList<T> implements IMyArrayList<T>{
+public class MyArrayList<T> implements IMyArrayList<T> {
     //дефолтный размер списка
     private final int INIT_SIZE = 2;
     //массив для элементов
+    // TODO можно сделать в консруторе без параметров.
     private Object[] listArray = new Object[INIT_SIZE];
     //счетчик элементов
     private int counter = 0;
@@ -11,40 +12,47 @@ public class MyArrayList<T> implements IMyArrayList<T>{
     /**
      * получает элемент по индексу из массива, если индекс
      * за пределами массива кидает исключение
+     *
      * @param index индекс элемента в массиве
      * @return возвращает элемент
      */
-    public T getItem(int index){
-        if(index < 0 || index >= listArray.length)
+    // FIXME возвращает null если в ячейке не лежит значение (возвращать null - плохо).
+    // FIXME метод может генериовать IndexOutOfBoundsException, но оператора throws нет!
+    public T getItem(int index) {
+        if (index < 0 || index >= listArray.length)
             throw new IndexOutOfBoundsException("Индекс за пределами массива");
-        return listArray[index] == null ? null : (T)listArray[index];
+        return listArray[index] == null ? null : (T) listArray[index];
     }
 
     /**
      * вставляет элемент в конец списка
+     *
      * @param value элемент, который мы вставляем
      */
-    public void putItem(T value){
-        if(counter == listArray.length-1)
-            resizeArray(listArray.length*2);
-        listArray[counter++]=value;
+    public void putItem(T value) {
+        if (counter == listArray.length - 1)
+            resizeArray(listArray.length * 2);
+        listArray[counter++] = value;
     }
 
     /**
      * метод, возвращающий размер списка
+     *
      * @return размер списка
      */
-    public int getSize(){
+    // FIXME возвращается размер внутреннего массива, а должно возвращатсья кол-во элементов списка (counter).
+    public int getSize() {
         return listArray.length;
     }
 
     /**
      * проверяет есть ли переданный элемент в списке
+     *
      * @param item элемент
      * @return возвращает true OR false в зависимости от того находит идентичный элемент или нет
      */
-    public boolean containsInArray(T item){
-        for(int i=0;i<listArray.length;++i)
+    public boolean containsInArray(T item) {
+        for (int i = 0; i < listArray.length; ++i)
             if (item.equals(listArray[i]))
                 return true;
         return false;
@@ -53,10 +61,12 @@ public class MyArrayList<T> implements IMyArrayList<T>{
     /**
      * метод очищающий список
      */
-    public void clearArray(){
+    // FIXME почему при создании листа внутренний массив инициализируется двумя элементами, а при очистке 0?
+    public void clearArray() {
         listArray = new Object[0];
     }
 
+    // TODO можно использовать Arrays.copyOf.
     //вспомогательный метод для ресайза массива
     private void resizeArray(int updateLength) {
         Object[] newArray = new Object[updateLength];
